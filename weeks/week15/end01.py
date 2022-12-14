0ae8e54810a440b05b7bd7aeb496b0a31f74b2b0
dapin <dapin1490@gmail.com>
2022-12-14 23:43:51 +0900

    정리중

--- weeks/week15/end01.py
+++ weeks/week15/end01.py
@@ -1,3 +1,21 @@
+"""
+나일강 흐름 관련 데이터인 nile.csv가 존재한다. 주어진 데이터는 나일강의 수심 깊이를 측정한 것이다. 해당 데이터를 histogram화 시키고 이와 비슷하게 도출되는 정규분포 식을 겹치게 하여 시각적으로 비교되게끔 plot하시요. 다만 하기 조건의 유의하시요. (5점)
+
+- 정규분포식을 직접 작성하시기 보다는 아래와 같은 코드를 활용하시요
+
+import scipy.stats as stats
+…………
+df_lins = np.linspace(df_min,df_max, 100)
+df_norm = stats.norm.pdf(df_lins, df_mean, df_std)
+
+상기에서 df_min과 df_max의 경우는 데이터내 최저점, 최고점을 뜻하며, df_mean과 df_std는 데이터 평균 및 표준편차를 의미한다.
+
+- plot이 두번 겹쳐 보이게 하기 위해서는 plt.plot()을 두번 연속으로 한 이후에 plt.show()를 하면 겹치게 보이는 점을 참고하세요.
+
+(5점)
+"""
+# 풀이 실패
+
 import pandas as pd
 import scipy.stats as stats
 import numpy as np
--- weeks/week15/end02.py
+++ weeks/week15/end02.py
@@ -1,3 +1,10 @@
+"""
+1959~1961년까지의 항공편수가 기록된 airtravel.csv가 존재한다. 해당 csv를 활용하여서 년도와 월별로 하여서 항공편수의 동향을 파악할 수 있는 colormap 혹은 heatmap을 출력하시요. 다만 다음 조건을 따르시요. (5점)
+- X, Y축 라벨이 정상적으로 입력되어야 한다.
+(5점)
+"""
+# 해결함
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
